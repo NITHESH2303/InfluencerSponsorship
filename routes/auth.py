ef629050e9799080472c01a9c6539308c28442e8
nithesh.kanna <nithesh.kanna@zohocorp.com>
2024-10-01 08:30:01 +0530

    Added API to password change

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,10 @@
 from flask_restful import Resource, reqparse
-from flask_security.utils import verify_password
+from flask_security import verify_password, auth_required, current_user
 from flask_jwt_extended import create_access_token
 from sqlalchemy import exc
 import re
-
+flask_security.change_email
+from application.database import db
 from application.models import User
 from application.response import *
 
@@ -15,8 +16,31 @@ class AuthAPI(Resource):
         self.auth_input_fields.add_argument("identifier", type=str, required=True, help="Enter Your User Or Email")
         self.auth_input_fields.add_argument("password", type=str, required=True, help="Password is required")
 
+    @auth_required()
     def get(self):
-        return
+        try:
+            user_data = current_user.to_dict(exclude=None)
+            return user_data
+        except exc.NoResultFound:
+            return unauthorized()
+
+    @auth_required()
+    def put(self):
+        args = self.auth_input_fields.parse_args()
+        identifier = args["identifier"]
+        password = args["password"]
+        try:
+            if not verify_password(password, current_user.password):
+                return validation_error("Incorrect Password")
+            if identifier:
+                if '@' in identifier and re.match(r"[^@]+@[^@]+\.[^@]+", identifier):
+                    current_user.email = identifier
+                else:
+                    current_user.username = identifier
+                db.session.commit()
+                return create_response("Username/Email updated successfully", 200)
+        except Exception as e:
+            return forbidden()
 
     def post(self):
         global inp_type
